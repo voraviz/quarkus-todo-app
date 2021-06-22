c53818e9ee499b0021666cc15afd7c173bde765e
voravitl <voravitl@gmail.com>
2021-06-22 20:31:32 +0700

    update pom

--- quarkus-todo/src/main/java/io/quarkus/sample/TodoResource.java
+++ quarkus-todo/src/main/java/io/quarkus/sample/TodoResource.java
@@ -5,14 +5,25 @@ import io.quarkus.panache.common.Sort;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import java.util.List;
-
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.ConcurrentGauge;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
 
 @Path("/api")
-@Produces("application/json")
-@Consumes("application/json")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@ConcurrentGauge(
+    name = "concurrentTodoResource",
+    description = "Concurrent connection to TodoResource"
+    )
 public class TodoResource {
 
     @OPTIONS
@@ -21,12 +32,36 @@ public class TodoResource {
     }
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "List All Tasks")
+    @APIResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON))
+    @Counted(
+        name = "countGetAll", 
+        description = "Counts how many times the getAll method has been invoked"
+        )
+    @Timed(
+        name = "timeGetAll", 
+        description = "Times how long it takes to invoke the getAll method in second", 
+        unit = MetricUnits.SECONDS
+        )
     public List<Todo> getAll() {
         return Todo.listAll(Sort.by("order"));
     }
 
     @GET
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "getOne Tasks")
+    @APIResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON))
+    @Counted(
+        name = "countGetOne", 
+        description = "Counts how many times the getOne method has been invoked"
+        )
+    @Timed(
+        name = "timeGetOne", 
+        description = "Times how long it takes to invoke the getOne method in second", 
+        unit = MetricUnits.SECONDS
+        )
     public Todo getOne(@PathParam("id") Long id) {
         Todo entity = Todo.findById(id);
         if (entity == null) {
@@ -44,6 +79,18 @@ public class TodoResource {
 
     @PATCH
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Update Task")
+    @APIResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON))
+    @Counted(
+        name = "countUpdate", 
+        description = "Counts how many times the update method has been invoked"
+        )
+    @Timed(
+        name = "timeUpdate", 
+        description = "Times how long it takes to invoke the update method in second", 
+        unit = MetricUnits.SECONDS
+        )
     @Transactional
     public Response update(@Valid Todo todo, @PathParam("id") Long id) {
         Todo entity = Todo.findById(id);
@@ -65,6 +112,18 @@ public class TodoResource {
     @DELETE
     @Transactional
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Delete Tasks")
+    @APIResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON))
+    @Counted(
+        name = "countDeleteOne", 
+        description = "Counts how many times the deleteOne method has been invoked"
+        )
+    @Timed(
+        name = "timeDeleteOne", 
+        description = "Times how long it takes to deleteOne the getAll method in second", 
+        unit = MetricUnits.SECONDS
+        )
     public Response deleteOne(@PathParam("id") Long id) {
         Todo entity = Todo.findById(id);
         if (entity == null) {
