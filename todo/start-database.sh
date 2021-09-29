f2bf77a3ea90b40cfe52650226393ed47be07d8d
voravitl <voravitl@gmail.com>
2021-09-29 19:07:13 +0700

    start DB

--- todo/start-database.sh
+++ todo/start-database.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
-docker run --ulimit memlock=-1:-1 -it --rm=true --memory-swappiness=0 \
+#!/bin/bash
+#docker run --ulimit memlock=-1:-1 -it --rm=true --memory-swappiness=0 \
+docker run --ulimit memlock=-1:-1 -it --rm=true  \
     --name todo-db -e POSTGRES_USER=todo \
     -e POSTGRES_PASSWORD=todoPassword -e POSTGRES_DB=todo \
     -p 5432:5432 postgres:13.1
