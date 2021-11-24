034d8ae23274ff62f5a3b1eb8a72c309aed06b52
voravitl <voravitl@gmail.com>
2021-11-24 14:03:33 +0700

    2.4.2

--- todo-reactive/build_jvm_container.sh
+++ todo-reactive/build_jvm_container.sh
@@ -2,6 +2,12 @@
 CONTAINER_NAME=todo-reactive
 TAG=v1
 TYPE=fast-jar
+CONTAINER_RUNTIME=podman
+podman --version 1>/dev/null 2>&1
+if [ $? -ne 0 ];
+then
+   CONTAINER_RUNTIME=docker 
+fi
 mvn clean package -DskipTests=true -Dquarkus.package.type=$TYPE
-docker build -f src/main/docker/Dockerfile.$TYPE \
+$CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.$TYPE \
 -t ${CONTAINER_NAME}:${TAG} .
--- todo-reactive/start-database.sh
+++ todo-reactive/start-database.sh
@@ -1,5 +1,12 @@
-#!/usr/bin/env bash
-docker run --ulimit memlock=-1:-1 -it --rm=true --memory-swappiness=0 \
-    --name postgres-quarkus-rest-http-crud -e POSTGRES_USER=restcrud \
-    -e POSTGRES_PASSWORD=restcrud -e POSTGRES_DB=rest-crud \
+#!/bin/bash
+#docker run --ulimit memlock=-1:-1 -it --rm=true --memory-swappiness=0 \
+CONTAINER_RUNTIME=podman
+podman --version 1>/dev/null 2>&1
+if [ $? -ne 0 ];
+then
+   CONTAINER_RUNTIME=docker 
+fi
+$CONTAINER_RUNTIME run --ulimit memlock=-1:-1 -it --rm=true  \
+    --name todo-db -e POSTGRES_USER=todo \
+    -e POSTGRES_PASSWORD=todoPassw0rd -e POSTGRES_DB=todo \
     -p 5432:5432 postgres:13.1
