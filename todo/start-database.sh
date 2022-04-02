aa33b6ab36a88b071868c37f596ac847e8e505d3
Voravit <voravitl@gmail.com>
2022-04-02 11:30:50 +0700

    podman

--- todo/start-database.sh
+++ todo/start-database.sh
@@ -6,7 +6,8 @@ if [ $? -ne 0 ];
 then
    CONTAINER_RUNTIME=docker 
 fi
-$CONTAINER_RUNTIME run --ulimit memlock=-1:-1 -it --rm=true  \
+#$CONTAINER_RUNTIME run --ulimit memlock=-1:-1 -it --rm=true  \
+$CONTAINER_RUNTIME run  --rm=true  \
     --name todo-db -e POSTGRES_USER=todo \
     -e POSTGRES_PASSWORD=todoPassw0rd -e POSTGRES_DB=todo \
     -p 5432:5432 postgres:13.1
