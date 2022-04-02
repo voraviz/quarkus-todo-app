3813f88f22d33b2751397fd2216bf7e1e8988d05
Voravit <voravitl@gmail.com>
2022-04-02 10:52:06 +0700

    2.7.5.Final

--- todo/build_jvm_container.sh
+++ todo/build_jvm_container.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 CONTAINER_NAME=todo
 TAG=v1
-TYPE=fast-jar
+TYPE=jvm
 CONTAINER_RUNTIME=podman
 podman --version 1>/dev/null 2>&1
 if [ $? -ne 0 ];
 then
    CONTAINER_RUNTIME=docker 
 fi
-mvn clean package -DskipTests=true -Dquarkus.package.type=$TYPE
+#mvn clean package -DskipTests=true -Dquarkus.package.type=$TYPE
+mvn clean package -DskipTests=true
 $CONTAINER_RUNTIME build -f src/main/docker/Dockerfile.$TYPE \
 -t ${CONTAINER_NAME}:${TAG} .
