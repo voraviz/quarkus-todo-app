d611bc849e4969b8f12a7982d2dd16f3dc45a5e0
Voraviz <voravitl@gmail.com>
2023-05-03 09:45:33 +0700

    Update test cases

--- todo/src/test/java/io/quarkus/sample/TodoResourceTest.java
+++ todo/src/test/java/io/quarkus/sample/TodoResourceTest.java
@@ -38,84 +38,84 @@ class TodoResourceTest {
                 .body("completed", is(true));
     }
 
-//     @Test
-//     @Order(2)
-//     void testAddingAnItem() {
-//         Todo todo = new Todo();
-//         todo.title = "testing the application";
-//         given()
-//                 .body(todo)
-//                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-//                 .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
-//                 .when()
-//                 .post("/api")
-//                 .then()
-//                 .statusCode(HttpStatus.SC_CREATED)
-//                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-//                 .body("title", is(todo.title))
-//                 .body("completed", is(false))
-//                 .body("id", is(5));
+    @Test
+    @Order(2)
+    void testAddingAnItem() {
+        Todo todo = new Todo();
+        todo.title = "testing the application";
+        given()
+                .body(todo)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .when()
+                .post("/api")
+                .then()
+                .statusCode(HttpStatus.SC_CREATED)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .body("title", is(todo.title))
+                .body("completed", is(false));
+                // .body("id", is(5));
 
-//         List<Todo> todos = get("/api").then()
-//                 .statusCode(HttpStatus.SC_OK)
-//                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-//                 .extract().body().as(getTodoTypeRef());
-//         assertEquals(5, todos.size());
-//     }
+        List<Todo> todos = get("/api").then()
+                .statusCode(HttpStatus.SC_OK)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .extract().body().as(getTodoTypeRef());
+        assertEquals(5, todos.size());
+    }
 
-//     @Test
-//     @Order(3)
-//     void testUpdatingAnItem() {
-//         Todo todo = new Todo();
-//         todo.title = "testing the application (updated)";
-//         todo.completed = true;
-//         given()
-//                 .body(todo)
-//                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-//                 .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
-//                 .pathParam("id", 5)
-//                 .when()
-//                 .patch("/api/{id}")
-//                 .then()
-//                 .statusCode(HttpStatus.SC_OK)
-//                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-//                 .body("title", is(todo.title))
-//                 .body("completed", is(true))
-//                 .body("id", is(5));
-//     }
+    @Test
+    @Order(3)
+    void testUpdatingAnItem() {
+        Todo todo = new Todo();
+        todo.title = "testing the application (updated)";
+        todo.completed = true;
+        given()
+                .body(todo)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .pathParam("id", 201)
+                .when()
+                .patch("/api/{id}")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .body("title", is(todo.title))
+                .body("completed", is(true))
+                .body("id", is(201));
+    }
 
-//     @Test
-//     @Order(4)
-//     void testDeletingAnItem() {
-//         given()
-//                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-//                 .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
-//                 .pathParam("id", 5)
-//                 .when()
-//                 .delete("/api/{id}")
-//                 .then()
-//                 .statusCode(HttpStatus.SC_NO_CONTENT);
+    @Test
+    @Order(4)
+    void testDeletingAnItem() {
+        given()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .pathParam("id", 201)
+                .when()
+                .delete("/api/{id}")
+                .then()
+                .statusCode(HttpStatus.SC_NO_CONTENT);
 
-//         List<Todo> todos = get("/api").then()
-//                 .statusCode(HttpStatus.SC_OK)
-//                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-//                 .extract().body().as(getTodoTypeRef());
-//         assertEquals(4, todos.size());
-//     }
+        List<Todo> todos = get("/api").then()
+                .statusCode(HttpStatus.SC_OK)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .extract().body().as(getTodoTypeRef());
+        assertEquals(4, todos.size());
+    }
 
-//     @Test
-//     @Order(5)
-//     void testDeleteCompleted() {
-//         delete("/api")
-//                 .then()
-//                 .statusCode(HttpStatus.SC_NO_CONTENT);
+    @Test
+    @Order(5)
+    void testDeleteCompleted() {
+        delete("/api")
+                .then()
+                .statusCode(HttpStatus.SC_NO_CONTENT);
 
-//         List<Todo> todos = get("/api").then()
-//                 .statusCode(HttpStatus.SC_OK)
-//                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-//                 .extract().body().as(getTodoTypeRef());
-//         assertEquals(3, todos.size());
-//     }
+        List<Todo> todos = get("/api").then()
+                .statusCode(HttpStatus.SC_OK)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .extract().body().as(getTodoTypeRef());
+        assertEquals(3, todos.size());
+    }
 
     private TypeRef<List<Todo>> getTodoTypeRef() {
         return new TypeRef<List<Todo>>() {
