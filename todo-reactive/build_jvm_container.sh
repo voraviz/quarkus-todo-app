b19cb21e975b24236e09fc9c21b384b3e909aee2
Voraviz <voravitl@gmail.com>
2023-07-12 08:04:18 +0700

    tag name

--- todo-reactive/build_jvm_container.sh
+++ todo-reactive/build_jvm_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CONTAINER_NAME=quay.io/voravitl/todo
-TAG=otel
+TAG=reactive-otel
 TYPE=jvm
 CONTAINER_RUNTIME=podman
 podman --version 1>/dev/null 2>&1
@@ -9,6 +9,6 @@ then
    CONTAINER_RUNTIME=docker 
 fi
 #mvn clean package -DskipTests=true -Dquarkus.package.type=$TYPE
-mvn clean package -DskipTests=true -DskipTests=true
+mvn clean package -DskipTests=true
 $CONTAINER_RUNTIME build --platform linux/amd64 -f src/main/docker/Dockerfile.$TYPE \
 -t ${CONTAINER_NAME}:${TAG} .
