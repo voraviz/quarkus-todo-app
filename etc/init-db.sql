49e35b0143e60a07f80935043484bd787fd719d9
Voraviz <voravitl@gmail.com>
2024-07-11 17:30:35 +0700

    Create sequence todo_seq

--- etc/init-db.sql
+++ etc/init-db.sql
@@ -1,5 +1,5 @@
 drop table if exists Todo;
-drop sequence if exists hibernate_sequence;
+drop sequence if exists todo_seq;
 create sequence todo_seq start 1 increment 1;
 create table Todo (
        id int8 not null,
@@ -11,7 +11,7 @@ create table Todo (
     );
 alter table if exists Todo
     add constraint unique_title unique (title);
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Introduction to Quarkus', true, 0, null);
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Hibernate with Panache', false, 1, null);
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Visit Quarkus web site', false, 2, 'https://quarkus.io');
-INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('hibernate_sequence'), 'Star Quarkus project', false, 3, 'https://github.com/quarkusio/quarkus/');
+INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('todo_seq'), 'Introduction to Quarkus', true, 0, null);
+INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('todo_seq'), 'Hibernate with Panache', false, 1, null);
+INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('todo_seq'), 'Visit Quarkus web site', false, 2, 'https://quarkus.io');
+INSERT INTO todo(id, title, completed, ordering, url) VALUES (nextval('todo_seq'), 'Star Quarkus project', false, 3, 'https://github.com/quarkusio/quarkus/');
