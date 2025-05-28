400245f7d054757431e0646cbc3233d267fadd6e
Voraviz <voravitl@gmail.com>
2025-05-28 07:44:36 +0700

    remove unused import

--- todo/src/main/java/io/quarkus/sample/TodoResource.java
+++ todo/src/main/java/io/quarkus/sample/TodoResource.java
@@ -16,8 +16,6 @@ import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
-
-import java.time.Instant;
 import java.util.List;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
