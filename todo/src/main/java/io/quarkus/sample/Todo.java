afbe5378ddaa812d462454ff85e9ef55f7e9c468
Voravit <voravitl@gmail.com>
2026-06-08 12:58:21 +0700

    add vlidation for max lenght of title and URL

--- todo/src/main/java/io/quarkus/sample/Todo.java
+++ todo/src/main/java/io/quarkus/sample/Todo.java
@@ -5,12 +5,14 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 
 @Entity
 public class Todo extends PanacheEntity {
 
     @NotBlank
+    @Size(min = 1, max = 1000, message = "Title must be between 1 and 1000 characters")
     @Column(unique = true)
     public String title;
 
@@ -19,6 +21,7 @@ public class Todo extends PanacheEntity {
     @Column(name = "ordering")
     public int order;
 
+    @Size(max = 4096, message = "URL must not exceed 4096 characters")
     public String url;
 
     public static List<Todo> findNotCompleted() {
